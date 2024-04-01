f2c9123df4f1dbb42968e5ab5fa7f5257822bfb7
Eric Eugene Carter <eric.eugene.carter@gmail.com>
2024-04-01 11:58:07 -0400

    Create footer MFE for Polyglot-Spa-V2

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+    //let bananaSrc = "https://polyglot-spa-footer.s3.us-east-2.amazonaws.com/assets/images/bananas.png"
+    let bananaSrc = "http://localhost:5002/assets/images/bananas.png"
+</script>
+
+<style>
+  #banana-png {
+      width: 200px;
+  }
+  #footer-text {
+      font-family: Bananas-Personal;
+      font-size: 50px;
+  }
+  #footer-link {
+      margin-top: 10px;
+  }
+</style>
+
+<div class="container-fluid pt-3">
+    <div>
+        <span id="footer-text" class="me-3">Do you like this font? Check out this artist!</span>
+        <a id="footer-link"  href="https://billyargel.com/" target="_blank" rel="noopener noreferrer">
+            <img id="banana-png" src={ bananaSrc } alt=""/>
+        </a>
+    </div>
+    <div class="mt-3">
+        <p class="float-end"><a href="#">Back to top</a></p>
+        <p>
+            Made by Eric Carter &middot; <a href="#">Privacy</a> &middot;
+            <a href="#">Terms</a>
+        </p>
+    </div>
+</div>
